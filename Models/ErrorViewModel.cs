08f35679d981e4bdbe8335287cc1ad7de62a8f49
Ana Durand <ana@juntoz.com>
2018-01-18 13:01:47 -0500

    new repo landing juntoz

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web.Chile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿$("#suscribirme").on("click", function () {
+    var genderTag = document.getElementsByName("gender");
+    var email = $("#email");
+    var errorText = $("#errorText");
+    var successText = $("#successText");
+    var gender = "";
+    var regexmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    for (var i = 0; i < genderTag.length; i++) {
+        if (genderTag[i].checked) {
+            gender = genderTag[i].value;
+            break;
+        }
+    }
+
+    if (gender != "" && email.val() != "") {
+        if (email.val().match(regexmail)) {
+            email.val(email.attr('placeholder'));
+            errorText.hide();
+            successText.show();
+        }
+        
+    } else {
+        errorText.show();
+        successText.hide();
+    }
+
+
+});
